c4afc5bcc05d577992b4359b5e5dcdfefd69bd4c
Sanyi93 <ttutudududududud@rututurututututututuutt.sk>
2025-03-25 18:23:51 -0400

    cartSlice created q

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -1,6 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
 
-const CartSlice = ({
+const CartSlice = createSlice ({
+    name: 'cart',
+    initialState,
+    //object with "reducer" functions; each key-value pair presents a single reducer
+    // where the key is the name of the action and the value is the reducer function
+    reducers: {
+     
+        //create functions here -> continue with step No 5
+
+    }
  
 });
 
-
+const initialState = {
+    const cartItems: [],
+};
