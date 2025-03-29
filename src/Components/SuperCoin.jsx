b3c1c1525bac5f8eace04e70d631e877ccec09e7
Sanyi93 <taaaaaaaararararara@tuuuururru.hu>
2025-03-29 18:46:07 -0400

    superCoin export

--- src/Components/SuperCoin.jsx
+++ src/Components/SuperCoin.jsx
@@ -26,4 +26,6 @@ return (
         <h2 className="super-coins-title">Super Coins</h2>
         <p className="super-coins-info">You will earn {superCoins} super coins with this purchase</p>
     </div>
-)
+);
+
+export default SuperCoin;
