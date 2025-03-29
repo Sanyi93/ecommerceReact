0b78bd61db9cc6631b656422e9e96490ac8479f9
Sanyi93 <taaaaaaaararararara@tuuuururru.hu>
2025-03-29 18:50:41 -0400

    supercoin changed

--- src/Components/SuperCoin.jsx
+++ src/Components/SuperCoin.jsx
@@ -1,15 +1,20 @@
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-const [superCoins, setSuperCoins ] = useState(0);
+
+const SuperCoin = () => {
+
+
+    const [superCoins, setSuperCoins ] = useState(0);
+
 
 //retrieving cartItems from the cart slice
-const cartItems = useSelector(state => state.cart.cartItems);
+    const cartItems = useSelector(state => state.cart.cartItems);
 
-const totalAmount = cartItems.reduce((total, item) => total + item.price*item.quantity, 0);
+    const totalAmount = cartItems.reduce((total, item) => total + item.price*item.quantity, 0);
 
 
 //using useEffect to set the high of superCoins everytime the value of [totalAmount] changes
-useEffect(() => {
+    useEffect(() => {
     if(totalAmount >= 100 && totalAmount <200) {
         setSuperCoins(10);
     } else if (totalAmount >= 200 && totalAmount < 300) {
@@ -19,13 +24,18 @@ useEffect(() => {
     } else {
         setSuperCoins(0);
     }
-}, [totalAmount]);
+    }, [totalAmount]);
 
-return (
+    return (
     <div className="super-coins" style={{textAlign: 'center'}}>
         <h2 className="super-coins-title">Super Coins</h2>
         <p className="super-coins-info">You will earn {superCoins} super coins with this purchase</p>
     </div>
-);
+    );
+
+
+
+}
+
 
 export default SuperCoin;
